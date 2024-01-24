3783483f1e646c0430403a0ee88586786727ea94
Will C IV <cleghornw04@proton.me>
2024-01-24 16:29:37 -0700

    init

---
+++ deployment.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33.0"
+    }
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+resource "aws_s3_bucket" "user_site_bucket" {
+  bucket_prefix = "user-site-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "user_site_bucket" {
+  bucket = aws_s3_bucket.user_site_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "user_site_bucket_acl" {
+  depends_on = [aws_s3_bucket.user_site_bucket]
+
+  bucket = aws_s3_bucket.user_site_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "static_user_site" {
+  bucket = aws_s3_bucket.user_site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "user_site_index" {
+  bucket  = aws_s3_bucket.user_site_bucket.id
+  key     = "index.html"
+  content = "<h1>Hello World</h1>"
+}
+
